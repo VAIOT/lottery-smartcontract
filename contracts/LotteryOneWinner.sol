471b2e392dac68e84385d72b54be7bb51c532e76
reddigszymon <mixowo@hotmail.com>
2022-11-23 13:50:09 +0100

    Fixed bug where people could win multiple times

--- contracts/LotteryOneWinner.sol
+++ contracts/LotteryOneWinner.sol
@@ -21,6 +21,7 @@ error Lottery__NotEnoughFundsSent();
 error Lottery__LotteryDoesNotExist();
 error Lottery__RewardProportionsError();
 error Lottery__RandomNumberAlreadyPicked();
+error Lottery__RandomNumberNotPicked();
 
 contract LotteryOneWinner is ReentrancyGuard, VRFConsumerBaseV2 {
     /* Lottery Variables */
@@ -98,9 +99,6 @@ contract LotteryOneWinner is ReentrancyGuard, VRFConsumerBaseV2 {
         if (_rewardProportions.length != _numOfWinners) {
             revert Lottery__NumOfPlayersNotEqualToNumOfRewards();
         }
-        if (idToLottery[lotteryId].author == _author || idToLottery[lotteryId].exists == true) {
-            revert Lottery__LotteryAlreadyExists();
-        }
         if (msg.value <= 0) {
             revert Lottery__NotEnoughFundsSent();
         }
@@ -114,8 +112,11 @@ contract LotteryOneWinner is ReentrancyGuard, VRFConsumerBaseV2 {
             revert Lottery__RewardProportionsError();
         }
 
-
         lotteryId += 1;
+
+        if (idToLottery[lotteryId].exists == true) {
+            revert Lottery__LotteryAlreadyExists();
+        }
 
         // Setting basic information about the lottery
 
@@ -185,23 +186,37 @@ contract LotteryOneWinner is ReentrancyGuard, VRFConsumerBaseV2 {
         uint256[] memory _randomWords
     ) internal override nonReentrant {
         idToRandomNumber[lotteryId] = _randomWords[0];
-        // uint256 indexOfWinner = _randomWords[0] % participants.length;
-        // address payable recentWinner = participants[indexOfWinner];
-        // s_winner = recentWinner;
-        // (bool success, ) = recentWinner.call{value: address(this).balance}("");
-        // if (!success) {
-            // revert Lottery__TransferFailed();
-        // }
         emit RandomNumberPicked(lotteryId, _randomWords[0]);
     }
 
     function payoutWinners(uint256 _lotteryId) public onlyOwner {
-        for (uint i=0; i<idToLottery[_lotteryId].numOfWinners; i++) {
-            uint256 vrfNumber = idToRandomNumber[_lotteryId];
-            uint256 randomNumber = uint256(keccak256(abi.encode(vrfNumber, block.timestamp, i))) % idToLottery[_lotteryId].participants.length;
-            address payable recentWinner = idToLottery[_lotteryId].participants[randomNumber];
-            idToLottery[_lotteryId].winners.push(recentWinner);
+        if (idToRandomNumber[_lotteryId] == 0) {
+            revert Lottery__RandomNumberNotPicked();
         }
+        if (idToLottery[_lotteryId].exists != true) {
+            revert Lottery__LotteryDoesNotExist();
+        }
+        if (idToLottery[_lotteryId].status != LotteryState.OPEN) {
+            revert Lottery__LotteryClosed();
+        }
+        idToLottery[_lotteryId].status = LotteryState.CLOSED;
+        uint256 vrfNumber = idToRandomNumber[_lotteryId];
+
+        // Creating a unique random number for every winner 
+        // 1. Create random number based on the array length
+        // 2. Find the winner and push it to a new array
+        // 3. Delete the previous winner and replace the last address with the deleted one
+        // 4. Delete the last element of the array which is empty and repeat the process
+
+        for (uint i=0; i<idToLottery[_lotteryId].numOfWinners; i++) {
+            uint256 randomIndex = uint256(keccak256(abi.encode(vrfNumber, block.timestamp, i))) % idToLottery[_lotteryId].participants.length;
+            address payable recentWinner = idToLottery[_lotteryId].participants[randomIndex];
+            idToLottery[_lotteryId].winners.push(recentWinner);
+            delete idToLottery[_lotteryId].participants[randomIndex];
+            idToLottery[_lotteryId].participants[randomIndex] = idToLottery[_lotteryId].participants[idToLottery[_lotteryId].participants.length-1];
+            idToLottery[_lotteryId].participants.pop();
+        }
+        // Loop over the array of winners and payout their winnings
         for (uint i=0; i<idToLottery[_lotteryId].winners.length; i++) {
             (bool success, ) = idToLottery[_lotteryId].winners[i].call{value: idToLottery[_lotteryId].finalRewards[i]}("");
             if (!success) {
@@ -251,10 +266,16 @@ contract LotteryOneWinner is ReentrancyGuard, VRFConsumerBaseV2 {
     function getLotteryInfo(uint256 _lotteryId) public view returns (Lottery memory) {
         return idToLottery[_lotteryId];
     }
+
     function getLotteryState(uint256 _lotteryId) public view returns (LotteryState) {
         return idToLottery[_lotteryId].status;
     }
+
     function getRandomNumber(uint256 _lotteryId) public view returns (uint256) {
         return idToRandomNumber[_lotteryId];
+    }
+
+    function getLotteryId() public view returns (uint256) {
+        return lotteryId;
     }
 }
